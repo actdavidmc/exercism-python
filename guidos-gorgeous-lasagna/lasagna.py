0bcd06b8b55ad03edb5980487ff3da288458d692
David Magaña Celis <act.davidmc@gmail.com>
2026-05-05 22:28:17 -0600

    chore(lasagna): clear comments

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -1,13 +1,3 @@
-"""Functions used in preparing Guido's gorgeous lasagna.
-
-Learn about Guido, the creator of the Python language:
-https://en.wikipedia.org/wiki/Guido_van_Rossum
-
-This is a module docstring, used to describe the functionality
-of a module and its functions and/or classes.
-"""
-
-
 EXPECTED_BAKE_TIME = 40
 PREPARATION_TIME = 2
 
