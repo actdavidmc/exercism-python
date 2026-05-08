f0e47a5d5ea5574246db06bae01f9816186cf8f7
David Magaña Celis <act.davidmc@gmail.com>
2026-05-07 21:19:53 -0600

    refactor(ghost): simplify boolean return expressions based on Pylint recommendations

--- ghost-gobble-arcade-game/arcade_game.py
+++ ghost-gobble-arcade-game/arcade_game.py
@@ -8,7 +8,7 @@ def eat_ghost(power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - can a ghost be eaten?
     """
-    return True if power_pellet_active and touching_ghost else False
+    return power_pellet_active and touching_ghost
 
 
 def score(touching_power_pellet, touching_dot):
@@ -18,7 +18,7 @@ def score(touching_power_pellet, touching_dot):
     :param touching_dot: bool - is the player touching a dot?
     :return: bool - has the player scored or not?
     """
-    return True if touching_power_pellet or touching_dot else False
+    return touching_power_pellet or touching_dot
 
 
 def lose(power_pellet_active, touching_ghost):
@@ -28,7 +28,7 @@ def lose(power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - has the player lost the game?
     """
-    return True if not power_pellet_active and touching_ghost else False
+    return not power_pellet_active and touching_ghost
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -39,4 +39,4 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - has the player won the game?
     """
-    return True if has_eaten_all_dots and not lose(power_pellet_active, touching_ghost) else False
+    return has_eaten_all_dots and not lose(power_pellet_active, touching_ghost)
